359554ccae03348b7bce3c2ffb9bdc5b41bf60e8
PAOLA FANTINEL DOS SANTOS <632110079@fspoa.br>
2023-10-09 20:35:54 -0300

    Inclusão das Classes

---
+++ CarCenter/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace CarCenter.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int YearFabrication { get; set; }
+        public int YearModel { get; set; }
+        public int Chassi { get; set; }
+        public float Price { get; set;  }
+
+    }
+}
---
+++ CarCenter/Models/Seller.cs
@@ -0,0 +1,13 @@
+﻿namespace CarCenter.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime AdmissionDate { get; set; }
+        public string Registration { get; set; }
+        public float Salary { get; set; }
+
+        // método calc comissão
+    }
+}
